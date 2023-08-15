f623104ae7382dbe7cf987e773e29f4d6ba5aa24
Pase Bukky <olubukunolapase@gmail.com>
2023-08-15 14:48:45 -0700

    Second update removing trailing whitespace

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,7 +4,7 @@
  * print_sign - Prints the sign of a number
  * @n: The number being checked
  *
- * Return: 1 if n is greater than zero, 
+ * Return: 1 if n is greater than zero,
  * 0 if n is zero, -1 if n is less than zero
  */
 int print_sign(int n)
