59edea6df40746a8161e36441b264c921b7e3a10
Pase Bukky <olubukunolapase@gmail.com>
2023-08-15 15:59:34 -0700

    Updating the function that prints the 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,27 +5,26 @@
  */
 void times_table(void)
 {
-    int a, b, product;
+	int a, b, product;
 
-    for (a = 0; a <= 9; a++)
-    {
-        for (b = 0; b <= 9; b++)
-        {
-            product = a * b;
-            if (b == 0)
-                _putchar('0' + product);
-            else
-            {
-                _putchar(',');
-                _putchar(' ');
-                if (product < 10)
-                    _putchar(' ');
-                else
-                    _putchar('0' + product / 10);
-                _putchar('0' + product % 10);
-            }
-        }
-        _putchar('\n');
-    }
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			product = a * b;
+			if (b == 0)
+				_putchar('0' + product);
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (product < 10)
+					_putchar(' ');
+				else
+					_putchar('0' + product / 10);
+				_putchar('0' + product % 10)
+			}
+		}
+		_putchar('\n');
+	}
 }
-
