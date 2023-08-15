06d590ef30df759b27e37f90094560d077b33bfe
Pase Bukky <olubukunolapase@gmail.com>
2023-08-15 07:42:47 -0700

    Displays a program that prints _putchar, followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,32 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int _putchar(char c);
+
+int main()
+{
+    /* Calling _putchar to print a script*/
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
+
+    return 0;
+}
+int _putchar (char c)
+{
+	return (write(1, &c, 1));
+}
