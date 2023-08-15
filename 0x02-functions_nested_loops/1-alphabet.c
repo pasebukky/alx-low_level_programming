767093cd2f0f715f0b8b8896e0b8fd15d470a072
Pase Bukky <olubukunolapase@gmail.com>
2023-08-15 10:29:30 -0700

    Third update to the function that prints the alphabet, in lowercase, followed by a new line

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,10 +8,9 @@
 
 void print_alphabet(void)
 {
-    char alphabet;
-    for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-    {
-        _putchar(alphabet);
-    }
-    _putchar('\n');
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)	
+	_putchar(alphabet);
+_putchar('\n');
 }
