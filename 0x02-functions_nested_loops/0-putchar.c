1bd58095e1622d2d333391bf1fd5fa4d6717620e
Pase Bukky <olubukunolapase@gmail.com>
2023-08-15 08:50:18 -0700

    Seventh update to the program that prints _putchar, followed by a new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,21 @@
 #include "main.h"
 
-/* Main is the entry point*/
+/* Main is the entry point 
+ * that prints _putchar
+ */
+
 int main(void)
 {
-    /* Calling _putchar to print a script*/
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
+ /* Calling _putchar to print a script*/
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
-    return (0);
+return (0);
 }
