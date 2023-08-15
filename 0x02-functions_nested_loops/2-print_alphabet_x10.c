8f24c9f1aebd104ec1e5accfb7a44ee211dc9d12
Pase Bukky <olubukunolapase@gmail.com>
2023-08-15 13:58:56 -0700

    Fourteenth update to the function that prints 10 times the alphabet, in lowercase, followed by a new line

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,22 +3,21 @@
 /**
  * print_alphabet_x10 - Prints lowercase alphabets 10 times
  */
-
 void print_alphabet_x10(void)
 {
-    char alphabet; /*Prints all lowercase alphabets*/
-    int multiples;
+	char alphabet; /* Prints lowercase alphabets*/
+	int multiples;
 
-    multiples = 0;
+	multiples = 0;
 
-    while (multiples < 10)
-    {
-        for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-        {
-            _putchar(alphabet);
-        }
-        _putchar('\n');
+	while (multiples < 10)
+	{
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		{
+			_putchar(alphabet);
+		}
+		_putchar('\n');
 
-        multiples++;
-    }
+		multiples++;
+	}
 }
