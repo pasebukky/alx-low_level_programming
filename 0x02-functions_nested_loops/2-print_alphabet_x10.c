a9cc9c2e0958be407cf2d91f175f52392339556a
Pase Bukky <olubukunolapase@gmail.com>
2023-08-15 12:26:37 -0700

    Fourth update to the function that prints 10 times the alphabet, in lowercase, followed by a new line

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -15,9 +15,9 @@ void print_alphabet_x10(void)
     {
         for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
         {
-            putchar(alphabet);
+           _putchar(alphabet);
         }
-        putchar('\n');
+        _putchar('\n');
 
         multiples++;
     }
