3f571de7ea7cb2f96fbb884b553805eeb089a148
Pase Bukky <olubukunolapase@gmail.com>
2023-08-15 15:39:29 -0700

    Secong update to the function that prints every minute of the day of Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,18 +5,18 @@
  */
 void jack_bauer(void)
 {
-    int hour, minute;
+	int hour, minute;
 
-    for (hour = 0; hour < 24; hour++)
-    {
-        for (minute = 0; minute < 60; minute++)
-        {
-            _putchar('0' + hour / 10);
-            _putchar('0' + hour % 10);
-            _putchar(':');
-            _putchar('0' + minute / 10);
-            _putchar('0' + minute % 10);
-            _putchar('\n');
-        }
-    }
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			_putchar('0' + hour / 10);
+			_putchar('0' + hour % 10);
+			_putchar(':');
+			_putchar('0' + minute / 10);
+			_putchar('0' + minute % 10);
+			_putchar('\n');
+		}
+	}
 }
