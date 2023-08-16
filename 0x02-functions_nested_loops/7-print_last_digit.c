82fc5f0aee7995749ff78d423f29ce7352f74110
Pase Bukky <olubukunolapase@gmail.com>
2023-08-15 17:00:14 -0700

    Third update to the function that prints the last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,12 +10,17 @@ int print_last_digit(int r)
 {
 	int last;
 
+	if (r == INT_MIN)
+	{
+		_putchar('0' + 8); /* Last digit of INT_MIN is 8*/
+		return 8;
+	}
+
 	if (r < 0)
 		r = -r;
-
+       
 	last = r % 10;
-	_putchar(last + '0');
-
-	return (last);
+	_putchar('0' + last);
+	
+	return last;
 }
-
