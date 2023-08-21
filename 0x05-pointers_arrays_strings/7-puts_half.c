7f1cac262a36352d3210393dfe105637a3ebdcdc
Pase Bukky <olubukunolapase@gmail.com>
2023-08-21 14:06:15 -0700

    Updating betty style code for task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,21 +6,16 @@
  */
 void puts_half(char *str)
 {
-    int length = 0;
-    char *start;
+int length = 0;
+char *start;
 
-    while (str[length] != '\0')
-    {
-        length++;
-    }
+while (str[length] != '\0')
+	length++;
 
-    start = str + (length + 1) / 2;
+start = str + (length + 1) / 2;
 
-    while (*start != '\0')
-    {
-        _putchar(*start);
+while (*start != '\0')
+	_putchar(*start);
         start++;
-    }
-
-    _putchar('\n');
+_putchar('\n');
 }
