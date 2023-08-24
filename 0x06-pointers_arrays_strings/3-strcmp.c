59d5c2602a2ef8644028a0c1920a99cfe212a2c2
Pase Bukky <olubukunolapase@gmail.com>
2023-08-24 16:56:45 -0700

    Betty coding update to task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,21 +2,20 @@
 #include <stdio.h>
 
 /**
- * _strcmp - Compares 2 strings with a loop.
- * @s1: First string
- * @s2: Second string
+ * _strcmp - Compares 2 strings using a loop.
+ * @s1: First string.
+ * @s2: Second string.
  *
- * Return: Negative,positive or zero value if s1 is
- * less than, greater than or equal to s2.
+ * Return: Negative, positive, or zero value if s1 is
+ * less than, greater than, or equal to s2.
  */
- 
+
 int _strcmp(char *s1, char *s2)
 {
-    for (; *s1 != '\0' && *s1 == *s2; s1++, s2++)
-    {
-        /* Continues the loop until the strings are different 
+for (; *s1 != '\0' && *s1 == *s2; s1++, s2++)
+{
+	/* Continues the loop until the strings are different
 	 * or until the end of the strings */
-    }
-
-    return (*s1 - *s2);
+}
+return (*s1 - *s2);
 }
