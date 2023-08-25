cb2b5dfceb70926918fab18bba63a9396aa45c1a
Pase Bukky <olubukunolapase@gmail.com>
2023-08-24 17:17:00 -0700

    Updating betty style coding for task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,31 +8,29 @@
  *
  * Return: Modified string pointer
  */
- 
+
 char *cap_string(char *str)
 {
-    char *ptr = str;
-    int q;
+char *ptr = str;
+int q;
 
-    for (q = 0; ptr[q] != '\0'; q++)
-    {
-        if (ptr[q] == ' ' || ptr[q] == '\t' || ptr[q] == '\n' ||
-            ptr[q] == ',' || ptr[q] == ';' || ptr[q] == '.' ||
-            ptr[q] == '!' || ptr[q] == '?' || ptr[q] == '"' ||
-            ptr[q] == '(' || ptr[q] == ')' || ptr[q] == '{' ||
-            ptr[q] == '}')
-        {
-            if (ptr[q + 1] >= 'a' && ptr[q + 1] <= 'z')
-            {
-                ptr[q + 1] = ptr[q + 1] - ('a' - 'A');
-            }
-        }
-    }
-
-    if (ptr[0] != '\0' && ptr[0] >= 'a' && ptr[0] <= 'z')
-    {
-        ptr[0] = ptr[0] - ('a' - 'A');
-    }
-
-    return str;
+for (q = 0; ptr[q] != '\0'; q++)
+{
+	if (ptr[q] == ' ' || ptr[q] == '\t' || ptr[q] == '\n' ||
+	    ptr[q] == ',' || ptr[q] == ';' || ptr[q] == '.' ||
+	    ptr[q] == '!' || ptr[q] == '?' || ptr[q] == '"' ||
+	    ptr[q] == '(' || ptr[q] == ')' || ptr[q] == '{' ||
+	    ptr[q] == '}')
+	{
+		if (ptr[q + 1] >= 'a' && ptr[q + 1] <= 'z')
+		{
+			ptr[q + 1] = ptr[q + 1] - ('a' - 'A');
+		}
+	}
+}
+	if (ptr[0] != '\0' && ptr[0] >= 'a' && ptr[0] <= 'z')
+	{
+		ptr[0] = ptr[0] - ('a' - 'A');
+	}
+return (str);
 }
