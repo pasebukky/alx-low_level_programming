9f1e74fad857cbf8315f6af388f22a9f1cf9b3d2
Pase Bukky <olubukunolapase@gmail.com>
2023-08-25 00:27:16 -0700

    Updating Betty style coding for task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,21 +7,17 @@
 
 void print_number(int n)
 {
-if (n == 0)
-{
-	_putchar('0');
-	return;
-}
-else if (n < 0)
+int p = 1;
+
+if (n < 0)
 {
 	_putchar('-');
 	n = -n;
 }
 
-int p = 1;
-
 while (n / p >= 10)
 	p *= 10;
+
 while (p != 0)
 {
 	_putchar((n / p) + '0');
