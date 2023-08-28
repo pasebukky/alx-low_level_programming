44864a9c14fc6b572dc9c0cc2b38e520c52f1eed
Pase Bukky <olubukunolapase@gmail.com>
2023-08-28 10:45:22 -0700

    Betty style correction to task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * _strchr - search for the first occurrence 
+ * _strchr - search for the first occurrence
  * of a specific character in a given string
  * @s: pointer to string being searched
  * @c: character being searched for
@@ -10,16 +10,16 @@
  * Return: Pointer to the first occurrence of 'c' in s
  * or NULL if 'c' is not found
  */
- 
+
 char *_strchr(char *s, char c)
 {
-    while (*s != c)
-    {
-        if (*s == '\0')
-        {
-            return (NULL);
-        }
-        s++;
-    }
-    return (s);
+while (*s != c)
+{
+	if (*s == '\0')
+	{
+		return (NULL);
+	}
+	s++;
+}
+return (s);
 }
