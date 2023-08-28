005ada2a28d002089a6a2f0ed50df2be44922b25
Pase Bukky <olubukunolapase@gmail.com>
2023-08-28 10:52:24 -0700

    Betty style correction to task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -2,37 +2,37 @@
 #include <stdio.h>
 
 /**
- * _strspn - find the length of the prefix of a 
+ * _strspn - find the length of the prefix of a
  * string that contains only certain characters.
  * @s: pointer to the string being searched
- * @accept: pointer to the string with certain 
+ * @accept: pointer to the string with certain
  * characters
  *
- * Return: Number of bytes in the initial segment of s which consist only of bytes from accept
+ * Return: Number of bytes in the initial segment
+ * of s which consist only of bytes from accept
  */
- 
+
 unsigned int _strspn(char *s, char *accept)
 {
-    unsigned int count = 0;
+unsigned int count = 0;
 
-    while (*s)
-    {
-        char *a = accept;
-        while (*a)
-        {
-            if (*s == *a)
-            {
-                count++;
-                break;
-            }
-            a++;
-        }
-        if (*a == '\0')
-        {
-            break;
-        }
-        s++;
-    }
-
-    return (count);
+while (*s)
+{
+	char *a = accept;
+	while (*a)
+	{
+		if (*s == *a)
+		{
+			count++;
+			break;
+		}
+		a++;
+	}
+	if (*a == '\0')
+	{
+		break;
+	}
+	s++
+}
+return (count);
 }
