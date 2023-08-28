6558676931a76082aad2200d66404aee73a9b819
Pase Bukky <olubukunolapase@gmail.com>
2023-08-28 11:23:10 -0700

    Betty style correction for task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,27 +10,25 @@
  * Return: Pointer to the beginning of the located
  * substring, or NULL if the substring is not found.
  */
- 
+
 char *_strstr(char *haystack, char *needle)
 {
-    while (*haystack)
-    {
-        char *h = haystack;
-        char *n = needle;
+while (*haystack)
+{
+	char *h = haystack;
+	char *n = needle;
 
-        while (*n != '\0' && *h == *n)
-        {
-            h++;
-            n++;
-        }
+	while (*n != '\0' && *h == *n)
+	{
+		h++;
+		n++;
+	}
 
-        if (*n == '\0')
-        {
-            return (haystack);
-        }
-
-        haystack++;
-    }
-
-    return (NULL);
+	if (*n == '\0')
+	{
+		return (haystack);
+	}
+	haystack++;
+}
+return (NULL);
 }
