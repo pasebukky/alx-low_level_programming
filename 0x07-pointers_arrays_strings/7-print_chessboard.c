8991e2970c9cb1f49ecdf9cad94dd5d7a791c5ce
Pase Bukky <olubukunolapase@gmail.com>
2023-08-28 11:28:46 -0700

    Betty style correction for task 6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -3,20 +3,20 @@
 
 /**
  * print_chessboard - prints a chessboard
- * @a: chessboard 2D array 
+ * @a: chessboard 2D array
  */
- 
+
 void print_chessboard(char (*a)[8])
 {
-    int p;
-    int q;
-    
-    for (p = 0; p < 8; p++)
-    {
-        for (q = 0; q < 8; q++)
-        {
-            _putchar(a[p][q]);
-        }
-        _putchar('\n');
-    }
+	int p;
+	int q;
+
+	for (p = 0; p < 8; p++)
+	{
+		for (q = 0; q < 8; q++)
+		{
+			_putchar(a[p][q]);
+		}
+		_putchar('\n');
+	}
 }
