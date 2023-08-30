2407216556a60ecc793813d2236546c55c14d44b
Pase Bukky <olubukunolapase@gmail.com>
2023-08-30 05:00:18 -0700

    Correction to task 8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -17,10 +17,10 @@ if (*s2 == '\0')
 if (*s2 == '*')
 {
 	if (*(s2 + 1) == '*')
-		return wildcmp(s1, s2 + 1);
-	return wildcmp(s1, s2 + 1) || (*s1 && wildcmp(s1 + 1, s2));
+		return (wildcmp(s1, s2 + 1));
+	return (wildcmp(s1, s2 + 1) || (*s1 && wildcmp(s1 + 1, s2)));
 }
 if (*s1 == *s2)
-	return wildcmp(s1 + 1, s2 + 1);
+	return (wildcmp(s1 + 1, s2 + 1));
 return (0);
 }
