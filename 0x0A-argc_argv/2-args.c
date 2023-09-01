7f35d46ea33b1a5e7d8a5c3dd1de3f57482905de
Pase Bukky <olubukunolapase@gmail.com>
2023-09-01 10:35:10 -0700

    Betty style correction to task 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -6,22 +6,22 @@
  * @argv: array of arguments
  * @argc: number of arguments
  *
- * Return: 0 
+ * Return: 0
  */
 
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[])
 {
-    int i;
-    
-    for (i = 0; i < argc; i++) 
-    {
-        char *arg = argv[i];
-        while (*arg != '\0') 
-        {
-            _putchar(*arg);
-            arg++;
-        }
-        _putchar('\n');
-    }
-    return 0;
+int i;
+
+for (i = 0; i < argc; i++)
+{
+	char *arg = argv[i];
+	while (*arg != '\0')
+	{
+		_putchar(*arg);
+		arg++;
+	}
+	_putchar('\n');
+}
+return (0);
 }
