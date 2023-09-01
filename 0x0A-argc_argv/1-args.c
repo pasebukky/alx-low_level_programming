8420f415d69ad5dec36a036ea242f44bf4f6d8f8
Pase Bukky <olubukunolapase@gmail.com>
2023-09-01 10:25:53 -0700

    Betty correction to task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,13 +6,12 @@
  * @argv: array of arguments
  * @argc: number of arguments
  *
- * Return: 0 
+ * Return: 0
  */
 
 int main(int argc, char *argv[])
 {
-    (void) argv; 
-    printf("%d\n", argc - 1);
-
-    return (0);
+	(void) argv;
+	printf("%d\n", argc - 1);
+return (0);
 }
