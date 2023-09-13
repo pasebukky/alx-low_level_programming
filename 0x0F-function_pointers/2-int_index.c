25eaf353e3a2b600a1703826b81fba7da4345c3d
Bukky Pase <olubukunolapase@yahoo.com>
2023-09-13 17:16:29 +0100

    Updated solution to task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,16 +16,15 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 int a;
 
-if (size <= 0)
+if (array == NULL || size <= 0 || cmp == NULL)
 	return (-1);
 
 for (a = 0; a < size; a++)
 {
-	if (cmp(array[a]) != 0)
+	if (cmp(array[a]))
 	{
 		return (a);
 	}
 }
-
 return (-1);
 }
