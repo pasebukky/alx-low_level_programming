ea4f5d0204bc707c1fdbd9df4d1c8125fd6e9723
Bukky Pase <olubukunolapase@yahoo.com>
2023-10-02 19:42:46 +0100

    Correction

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,81 +1,83 @@
 #include "main.h"
 
-/**
- * open_file - Opens a file with specified flags and mode, handles errors.
- * @filename: name of the file to open.
- * @flags: flags for opening the file.
- * @mode: mode to set if creating the file.
- *
- * Return: file descriptor on success, or exits on failure.
- */
+int open_file(const char *filename, int flags, mode_t mode);
+void copy_file(const char *file_from, const char *file_to);
 
 int open_file(const char *filename, int flags, mode_t mode)
 {
-int fd = open(filename, flags, mode);
+	int fd = open(filename, flags, mode);
 
-if (fd == -1)
-{
-	dprintf(STDERR_FILENO, "Error: Can't open file %s\n", filename);
-	exit(98);
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't open file %s\n", filename);
+		exit(98);
+	}
+	return (fd);
 }
-return (fd);
-}
-
-/**
- * copy_file - Copies the contents of one file to another.
- * @file_from: source file to copy from.
- * @file_to: destination file to copy to.
- */
 
 void copy_file(const char *file_from, const char *file_to)
 {
-int file_from_fd, file_to_fd;
-ssize_t text_read, text_written;
-char *buffer;
+	int src_file, dest_file;
+	ssize_t text_read, text_written;
+	char *buffer = malloc(1024);
 
-file_from_fd = open_file(file_from, O_RDONLY, 0);
-file_to_fd = open_file(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR |
-S_IWUSR | S_IRGRP | S_IROTH);
-
-buffer = malloc(1024);
-
-if (buffer == NULL)
-{
-	dprintf(STDERR_FILENO, "Error: Memory allocation failed\n");
-	exit(100);
-}
-
-while ((text_read = read(file_from_fd, buffer, 1024)) > 0)
-{
-	text_written = write(file_to_fd, buffer, text_read);
-
-	if (text_written != text_read)
+	if (buffer == NULL)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", file_to);
-		exit(99);
+		dprintf(STDERR_FILENO, "Error: Memory allocation failed\n");
+		exit(100);
+	}
+
+	src_file = open_file(file_from, O_RDONLY, 0);
+	dest_file = open_file(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+
+	while ((text_read = read(src_file, buffer, 1024)) > 0)
+	{
+		if (text_read == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+			free(buffer);
+			close(src_file);
+			close(dest_file);
+			exit(98);
+		}
+
+		text_written = write(dest_file, buffer, text_read);
+
+		if (text_written == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", file_to);
+			free(buffer);
+			close(src_file);
+			close(dest_file);
+			exit(99);
+		}
+	}
+
+	free(buffer);
+
+	if (close(src_file) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", src_file);
+		exit(100);
+	}
+
+	if (close(dest_file) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", dest_file);
+		exit(100);
 	}
 }
-free(buffer);
-close(file_from_fd);
-close(file_to_fd);
-}
-
-/**
- * main - Entry point of the program.
- * @argc: Number of command-line arguments.
- * @argv: Array of command-line argument strings.
- *
- * Return: 0 on success, or exits with an error code.
- */
 
 int main(int argc, char *argv[])
 {
-if (argc != 3)
-{
-	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-	exit(97);
-}
-copy_file(argv[1], argv[2]);
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
 
-return (0);
+	copy_file(argv[1], argv[2]);
+
+	return (0);
 }
+
