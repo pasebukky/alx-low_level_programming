9b6b6f4721b78ab55d602a6addfaa0ea230e6ba5
Bukky Pase <olubukunolapase@yahoo.com>
2023-10-02 17:51:00 +0100

    Change of entire code format

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * error_exit - prints an error message and exits with a specified code.
- * @message: error message
- * @exit_code: exit code used when exiting.
+ * error_exit - Prints an error message and exits with the specified code.
+ * @message: The error message to print.
+ * @exit_code: The exit code to use when exiting
  */
 
 void error_exit(const char *message, int exit_code)
@@ -13,79 +13,84 @@ exit(exit_code);
 }
 
 /**
- * open_file - opens a file with specified flags and mode, handles errors.
- * @filename: name of the file to open.
- * @flags: flags for opening the file.
- * @mode: mode to set if creating the file.
+ * open_and_create_files - Opens source and destination files.
+ * @file_from: The name of the source file.
+ * @file_to: The name of the destination file.
+ * @source_fd: Pointer to store the source file descriptor.
+ * @destination_fd: Pointer to store the destination file descriptor.
  *
- * Return: file descriptor on success, or exits on failure.
+ * Return: 0 on success, -1 on failure.
  */
 
-int open_file(const char *filename, int flags, mode_t mode)
+int open_and_create_files(const char *file_from, const char *file_to, int *source_fd, int *destination_fd)
 {
-int fd = open(filename, flags, mode);
+*source_fd = open(file_from, O_RDONLY);
+if (*source_fd == -1)
+{
+    return (-1);
+}
 
-if (fd == -1)
+*destination_fd = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+if (*destination_fd == -1)
 {
-	error_exit("Error: Can't open file", 98);
+    return (-1);
 }
-return (fd);
+return (0);
 }
+
 
 /**
- * copy_file - copies the content of one file to another.
- * @file_from: source file to copy from.
- * @file_to: destination file to copy to.
+ * copy_data - Copies data from source file to destination file.
+ * @source_fd: The source file descriptor.
+ * @destination_fd: The destination file descriptor.
  */
 
-void copy_file(const char *file_from, const char *file_to)
+void copy_data(int source_fd, int destination_fd)
 {
-int file_from_fd, file_to_fd;
-char *content;
-ssize_t text_read, text_written;
+char buffer[BUFFER_SIZE];
+ssize_t bytes_read, bytes_written;
 
-file_from_fd = open_file(file_from, O_RDONLY, 0);
-file_to_fd = open_file(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR |
-S_IWUSR | S_IRGRP | S_IROTH);
-
-content = malloc(BUFFER_SIZE);
-
-if (content == NULL)
+while ((bytes_read = read(source_fd, buffer, BUFFER_SIZE)) > 0)
 {
-	error_exit("Error: Memory allocation failed", 100);
+    bytes_written = write(destination_fd, buffer, bytes_read);
+    if (bytes_written != bytes_read)
+    {
+        error_exit("Error: Can't write to destination file", 99);
+    }
+}
 }
 
-while ((text_read = read(file_from_fd, content, BUFFER_SIZE)) > 0)
-{
-	text_written = write(file_to_fd, content, text_read);
-	if (text_written != text_read)
-	{
-		error_exit("Error: Can't write to file", 99);
-	}
-}
-
-free(content);
-
-close(file_from_fd);
-close(file_to_fd);
-}
 
 /**
- * main - entry point of the program.
- * @argc:number of command-line arguments.
- * @argv: array of command-line argument strings.
- *
- * Return: 0 on success, or exits with an error code.
+ * close_files - Closes source and destination files.
+ * @source_fd: The source file descriptor.
+ * @destination_fd: The destination file descriptor.
  */
+
+void close_files(int source_fd, int destination_fd)
+{
+close(source_fd);
+close(destination_fd);
+}
 
 int main(int argc, char *argv[])
 {
+int source_fd, destination_fd;
+
 if (argc != 3)
 {
-	error_exit("Usage: cp file_from file_to\n", 97);
+	error_exit("Usage: cp file_from file_to", 97);
 }
 
-copy_file(argv[1], argv[2]);
+if (open_and_create_files(argv[1], argv[2], &source_fd, &destination_fd) == -1)
+{
+	error_exit("Error opening or creating files", 99);
+}
+
+copy_data(source_fd, destination_fd);
+
+close_files(source_fd, destination_fd);
 
 return (0);
 }
+
