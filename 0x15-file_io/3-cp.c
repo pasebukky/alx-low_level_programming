46c9be02a696b97918ff94cd3835df14cfafbfce
Bukky Pase <olubukunolapase@yahoo.com>
2023-10-02 19:17:34 +0100

    Correction to task 3 showing buffer of 1024 size and error code 100

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,96 +1,81 @@
 #include "main.h"
 
 /**
- * error_exit - Prints an error message and exits with the specified code.
- * @message: The error message to print.
- * @exit_code: The exit code to use when exiting
- */
-
-void error_exit(const char *message, int exit_code)
-{
-dprintf(STDERR_FILENO, "%s\n", message);
-exit(exit_code);
-}
-
-/**
- * open_and_create_files - Opens source and destination files.
- * @file_from: The name of the source file.
- * @file_to: The name of the destination file.
- * @source_fd: Pointer to store the source file descriptor.
- * @destination_fd: Pointer to store the destination file descriptor.
+ * open_file - Opens a file with specified flags and mode, handles errors.
+ * @filename: name of the file to open.
+ * @flags: flags for opening the file.
+ * @mode: mode to set if creating the file.
  *
- * Return: 0 on success, -1 on failure.
+ * Return: file descriptor on success, or exits on failure.
  */
 
-int open_and_create_files(const char *file_from, const char *file_to, int *source_fd, int *destination_fd)
+int open_file(const char *filename, int flags, mode_t mode)
 {
-*source_fd = open(file_from, O_RDONLY);
-if (*source_fd == -1)
-{
-    return (-1);
-}
+int fd = open(filename, flags, mode);
 
-*destination_fd = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-if (*destination_fd == -1)
+if (fd == -1)
 {
-    return (-1);
+	dprintf(STDERR_FILENO, "Error: Can't open file %s\n", filename);
+	exit(98);
 }
-return (0);
+return (fd);
 }
-
 
 /**
- * copy_data - Copies data from source file to destination file.
- * @source_fd: The source file descriptor.
- * @destination_fd: The destination file descriptor.
+ * copy_file - Copies the contents of one file to another.
+ * @file_from: source file to copy from.
+ * @file_to: destination file to copy to.
  */
 
-void copy_data(int source_fd, int destination_fd)
+void copy_file(const char *file_from, const char *file_to)
 {
-char buffer[BUFFER_SIZE];
-ssize_t bytes_read, bytes_written;
+int file_from_fd, file_to_fd;
+ssize_t text_read, text_written;
+char *buffer;
 
-while ((bytes_read = read(source_fd, buffer, BUFFER_SIZE)) > 0)
+file_from_fd = open_file(file_from, O_RDONLY, 0);
+file_to_fd = open_file(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR |
+S_IWUSR | S_IRGRP | S_IROTH);
+
+buffer = malloc(1024);
+
+if (buffer == NULL)
 {
-    bytes_written = write(destination_fd, buffer, bytes_read);
-    if (bytes_written != bytes_read)
-    {
-        error_exit("Error: Can't write to destination file", 99);
-    }
-}
+	dprintf(STDERR_FILENO, "Error: Memory allocation failed\n");
+	exit(100);
 }
 
+while ((text_read = read(file_from_fd, buffer, 1024)) > 0)
+{
+	text_written = write(file_to_fd, buffer, text_read);
+
+	if (text_written != text_read)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", file_to);
+		exit(99);
+	}
+}
+free(buffer);
+close(file_from_fd);
+close(file_to_fd);
+}
 
 /**
- * close_files - Closes source and destination files.
- * @source_fd: The source file descriptor.
- * @destination_fd: The destination file descriptor.
+ * main - Entry point of the program.
+ * @argc: Number of command-line arguments.
+ * @argv: Array of command-line argument strings.
+ *
+ * Return: 0 on success, or exits with an error code.
  */
-
-void close_files(int source_fd, int destination_fd)
-{
-close(source_fd);
-close(destination_fd);
-}
 
 int main(int argc, char *argv[])
 {
-int source_fd, destination_fd;
-
 if (argc != 3)
 {
-	error_exit("Usage: cp file_from file_to", 97);
+	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+	exit(97);
 }
-
-if (open_and_create_files(argv[1], argv[2], &source_fd, &destination_fd) == -1)
-{
-	error_exit("Error opening or creating files", 99);
-}
-
-copy_data(source_fd, destination_fd);
-
-close_files(source_fd, destination_fd);
+copy_file(argv[1], argv[2]);
 
 return (0);
 }
-
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,13 +9,10 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-#define BUFFER_SIZE 1024
-
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void error_exit(const char *message, int exit_code);
 int open_file(const char *filename, int flags, mode_t mode);
 void copy_file(const char *file_from, const char *file_to);
 
