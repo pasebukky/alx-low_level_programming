464ac1a1322df38a3b5e60fccd0340b5f4f3b206
Bukky Pase <olubukunolapase@yahoo.com>
2023-10-03 06:59:57 +0100

    Updated task

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -18,7 +18,17 @@ int append_text_to_file(const char *filename, char *text_content);
 int open_file(const char *filename, int flags, mode_t mode);
 void copy_file(const char *file_from, const char *file_to);
 void print_error_and_exit(const char *message);
-void print_elf_info(Elf64_Ehdr *elf_header);
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
+
 
 
 #endif
