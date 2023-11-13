eb2574abe05435bbcc9e2f23e4cfae05ad1dfc87
pasebukky <olubukunolapase@gmail.com>
2023-11-13 16:45:38 +0100

    Create a dynamic library that contains C functions that can be called from Python

---
+++ 0x18-dynamic_libraries/tests.c
@@ -0,0 +1,74 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - adds to integers
+ * @a: first integer
+ * @b: second integer
+ * Return: Answer after addition
+ *
+ */
+
+int add(int a, int b)
+{
+return (a + b);
+}
+
+
+
+/**
+ * sub - subracts two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: Answer after subtraction
+ */
+int sub(int a, int b)
+{
+return (a - b);
+}
+
+
+
+
+
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: Answer after multiplication
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
+
+
+
+
+
+/**
+ * div - divides two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: Answer after division without remainder
+ */
+int div(int a, int b)
+{
+return (a / b);
+}
+
+
+
+
+
+/**
+ * mod - modulus of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: Answer after modulus operation 
+ */
+int mod(int a, int b)
+{
+return (a % b);
+}
+
