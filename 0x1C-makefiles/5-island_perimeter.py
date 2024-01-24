e153fec280b2f5d0784f30a554afb08cb3fd46e6
pasebukky <olubukunolapase@gmail.com>
2024-01-24 11:56:53 +0100

    Create makefile and technical interview py

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                perimeter += 1 if i == 0 or grid[i-1][j] == 0 else 0
+                perimeter += 1 if j == 0 or grid[i][j-1] == 0 else 0
+                perimeter += 1 if i == len(grid)-1 or grid[i+1][j] == 0 else 0
+                perimeter += 1 if j == len(grid[0])-1 or grid[i][j+1] == 0 \
+                    else 0
+    return perimeter
